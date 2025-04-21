6209f188cd5d89445021db479cca401bdebf69ff
Aravind Singh Bisht <166579628+aravindbisht@users.noreply.github.com>
2025-04-20 17:05:36 -0700

    Update __init__.py

--- mmdet3d/models/dense_heads/__init__.py
+++ mmdet3d/models/dense_heads/__init__.py
@@ -19,6 +19,7 @@ from .smoke_mono3d_head import SMOKEMono3DHead
 from .ssd_3d_head import SSD3DHead
 from .vote_head import VoteHead
 from .voxelnext_head import VoxelNeXtHead
+from .optimized_voxelnext_head import OptimizedVoxelNeXtHead
 
 __all__ = [
     'Anchor3DHead', 'FreeAnchor3DHead', 'PartA2RPNHead', 'VoteHead',
@@ -26,5 +27,5 @@ __all__ = [
     'BaseMono3DDenseHead', 'AnchorFreeMono3DHead', 'FCOSMono3DHead',
     'GroupFree3DHead', 'PointRPNHead', 'SMOKEMono3DHead', 'PGDHead',
     'MonoFlexHead', 'Base3DDenseHead', 'FCAF3DHead', 'ImVoxelHead',
-    'VoxelNeXtHead'
+    'VoxelNeXtHead','OptimizedVoxelNeXtHead'
 ]
