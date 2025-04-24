29e70e51d7086a92fe3c31832264d9a98f947169
Aravind Singh Bisht <166579628+aravindbisht@users.noreply.github.com>
2025-04-23 23:40:16 -0700

    Update voxelnext_head.py

--- mmdet3d/models/dense_heads/voxelnext_head.py
+++ mmdet3d/models/dense_heads/voxelnext_head.py
@@ -20,21 +20,27 @@ class VoxelNeXtHead(CenterHead):
                  pc_range: List[float] = [0, -40, -3, 70.4, 40, 1],
                  voxel_size: List[float] = [0.05, 0.05, 0.1],
                  out_stride: int = 8,
+                 with_velocity: bool = False,
                  **kwargs):
 
         tasks = (dict(num_class=num_classes,
                       class_names=['Pedestrian', 'Cyclist', 'Car']),)
 
+        common_heads = dict(
+            center=(2, feat_channels),
+            center_z=(1, feat_channels),
+            dim=(3, feat_channels),
+            rot=(2, feat_channels))
+        code_size = 7
+        if with_velocity:
+            common_heads['vel'] = (2, feat_channels)
+            code_size = 9
+
         super().__init__(
             in_channels=in_channels,
             tasks=tasks,
             share_conv_channel=feat_channels,
-            common_heads=dict(
-                center=(2, feat_channels),
-                center_z=(1, feat_channels),
-                dim=(3, feat_channels),
-                rot=(2, feat_channels),
-                vel=(2, feat_channels)),
+            common_heads=common_heads,
             separate_head=dict(type='SeparateHead', init_bias=-2.19, final_kernel=3),
             bbox_coder=dict(
                 type='CenterPointBBoxCoder',
@@ -44,7 +50,7 @@ class VoxelNeXtHead(CenterHead):
                 score_threshold=0.1,
                 out_size_factor=out_stride,
                 voxel_size=voxel_size,
-                code_size=9),
+                code_size=code_size),
             **kwargs)
 
         # default Gaussian focal / L1 losses defined in parent
