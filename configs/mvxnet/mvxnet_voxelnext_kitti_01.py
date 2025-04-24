8a529febc0abb8673fa4641ed57e7a54d89587c8
Aravind Singh Bisht <166579628+aravindbisht@users.noreply.github.com>
2025-04-23 23:02:08 -0700

    Update mvxnet_voxelnext_kitti_01.py

--- configs/mvxnet/mvxnet_voxelnext_kitti_01.py
+++ configs/mvxnet/mvxnet_voxelnext_kitti_01.py
@@ -89,7 +89,6 @@ model = dict(
             min_radius=2,
             out_size_factor=8,
             code_weights=[1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0])),
-    train_cfg=dict(pts=None),
     test_cfg=dict(
         pts=dict(
             use_rotate_nms=True,
@@ -254,4 +253,4 @@ visualizer = dict(
     type='Det3DLocalVisualizer', vis_backends=vis_backends, name='visualizer')
 
 # Default setting for scaling LR automatically
-auto_scale_lr = dict(base_batch_size=16)
+auto_scale_lr = dict(base_batch_size=4)
