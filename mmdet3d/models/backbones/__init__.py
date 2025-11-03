ee4d4be569f7a227c604a91978d9cc080e7b416b
Ubuntu <ubuntu@ip-172-31-22-22.us-west-1.compute.internal>
2025-11-03 20:12:23 +0000

    feat: 2d version for 2d backbone

--- mmdet3d/models/backbones/__init__.py
+++ mmdet3d/models/backbones/__init__.py
@@ -4,7 +4,7 @@ from mmdet.models.backbones import SSDVGG, HRNet, ResNet, ResNetV1d, ResNeXt
 from .cylinder3d import Asymm3DSpconv
 from .dgcnn import DGCNNBackbone
 from .dla import DLANet
-from .fire_rpfnet import FireRPFNet, FireRPFNetV2
+from .fire_rpfnet import FireRPFNet, FireRPFNetV2, FireRPFNet2D
 from .mink_resnet import MinkResNet
 from .minkunet_backbone import MinkUNetBackbone
 from .multi_backbone import MultiBackbone
@@ -21,5 +21,5 @@ __all__ = [
     'SECOND', 'DGCNNBackbone', 'PointNet2SASSG', 'PointNet2SAMSG',
     'MultiBackbone', 'DLANet', 'MinkResNet', 'Asymm3DSpconv',
     'MinkUNetBackbone', 'SPVCNNBackbone', 'MinkUNetBackboneV2','SQUEEZE',
-    'RPFNet', 'FireRPFNet', 'FireRPFNetV2'
+    'RPFNet', 'FireRPFNet', 'FireRPFNetV2', 'FireRPFNet2D'
 ]
